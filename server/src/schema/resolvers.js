75544203609a6a48e38216cd0bf6032c7b51b070
Queen McSteve <niko.desilva@esmt.org>
2022-03-12 15:24:34 +0100

    rework resolver to talk to db

--- server/src/schema/resolvers.js
+++ server/src/schema/resolvers.js
@@ -1,21 +1,22 @@
-
+const Course = require("../models/Course");
 
 const resolvers = {
-    Query: {
-        hello: () => {
-            return "Hello World";
-        },
-        courses: () => {
-            return [{id: 1, name: "math", ects: 2, spots: 15 }, {id: 2, name: "science", ects: 2, spots: 15 }, {id: 3, name: "english", ects: 2, spots: 30 }]
-        }
+  Query: {
+    hello: () => {
+      return "Hello World";
     },
+    courses: async () => {
+      return await Course.find({});
+      // return [{id: 1, name: "math", ects: 2, spots: 15 }, {id: 2, name: "science", ects: 2, spots: 15 }, {id: 3, name: "english", ects: 2, spots: 30 }]
+    },
+  },
 
-    Mutation: {
-        addRanking: (parent, args)=> {
-            console.log("Received ", args);
-            return true;
-        }
-    }
+  Mutation: {
+    addRanking: (parent, args) => {
+      console.log("Received ", args);
+      return true;
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/src/seeds/seed.js
+++ server/src/seeds/seed.js
@@ -1,5 +1,16 @@
-const Course = require('../models/Course.js');
+const db = require("../config/connection");
+const Course = require("../models/Course");
 
-const courseData = require('./courseData.json');
+const courseData = require("./courseData.json");
 
+db.once("open", async () => {
+  await Course.deleteMany({});
 
+  const courses = await Course.insertMany(courseData);
+
+  for (newCourse of courses) {
+  }
+
+  console.log("all done!");
+  process.exit(0);
+});
