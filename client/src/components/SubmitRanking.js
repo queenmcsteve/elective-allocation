a90094cf61c7b914a586536cb42b943a5817fa7c
Queen McSteve <niko.desilva@esmt.org>
2022-03-18 13:39:26 +0100

    add success.js

--- client/src/components/SubmitRanking.js
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-const SUBMIT_RANKING = gql`
-  mutation ($ranking: [CourseChoice]!) {
-    addRanking(ranking: $ranking)
-  }
-`;
-
-const SubmitRanking = () => {};
---
+++ client/src/components/Success.js
@@ -0,0 +1,5 @@
+const Success = () => {
+  return <div>your ranking has been submitted!</div>;
+};
+
+export default Success;
