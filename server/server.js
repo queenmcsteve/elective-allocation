0d532adb4a716802ffc5f1e07ae35e55830d06b6
Queen McSteve <niko.desilva@esmt.org>
2022-03-26 09:45:20 +0100

    update for heroku deploy

--- server/server.js
+++ server/server.js
@@ -25,6 +25,11 @@ serverStart();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
 db.once("open", () => {
   try {
     app.listen(PORT, () => {
