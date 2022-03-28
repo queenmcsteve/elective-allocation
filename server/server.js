84b8bf2a52f24a3100816465cbffce1ee97f56ad
Queen McSteve <niko.desilva@esmt.org>
2022-03-28 20:48:09 +0200

    add app.get line 32 in server

--- server/server.js
+++ server/server.js
@@ -29,6 +29,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 db.once("open", () => {
   try {
     app.listen(PORT, () => {
