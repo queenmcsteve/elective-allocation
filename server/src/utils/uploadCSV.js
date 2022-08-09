e19de334af51dd0d54b8dacbd9acb8390934ccf5
Queen McSteve <niko.desilva@esmt.org>
2022-08-09 16:18:42 +0200

    update uploadCSV.js to parse course data

--- server/src/utils/uploadCSV.js
+++ server/src/utils/uploadCSV.js
@@ -1,3 +1,21 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+const data = [];
+
+fs.createReadStream("./RankMatch_courses.csv")
+  .pipe(parse({ delimiter: ",", columns: true, ltrim: true }))
+  .on("data", function (row) {
+    console.log(row);
+    data.push(row);
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  })
+  .on("end", function () {
+    console.log("finished");
+  });
+
 const uploadCsv = (courses) => {
   const courseCsv = courses;
 };
