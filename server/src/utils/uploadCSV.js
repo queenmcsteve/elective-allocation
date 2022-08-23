88ccd9ff919b3c9db66dc29a1bb5f6488dec9305
Queen McSteve <niko.desilva@esmt.org>
2022-08-23 14:18:47 +0200

    add function to parse and load student data to uploadCSV.js

--- server/src/utils/uploadCSV.js
+++ server/src/utils/uploadCSV.js
@@ -3,40 +3,71 @@ const { parse } = require("csv-parse");
 const db = require("../config/connection");
 const Course = require("../models/Course");
 const Student = require("../models/Student");
-const Admin = require("../models/Admin");
+// const Admin = require("../models/Admin");
 
 const data = [];
 
-fs.createReadStream("./RankMatch_courses.csv")
-  .pipe(parse({ delimiter: ",", columns: true, ltrim: true }))
-  .on("data", function (row) {
-    data.push(row);
-  })
-  .on("error", function (error) {
-    console.log(error.message);
-  })
-  .on("end", function () {
-    console.log(data);
-    console.log("finished");
-  });
+let courseData = "./RankMatch_courses.csv";
+let studentData = "./RankMatch_students.csv";
 
-db.once("open", async () => {
-  try {
-    await Course.deleteMany({});
+const uploadCourseData = (courseData) => {
+  fs.createReadStream(courseData)
+    .pipe(parse({ delimiter: ",", columns: true, ltrim: true }))
+    .on("data", function (row) {
+      data.push(row);
+    })
+    .on("error", function (error) {
+      console.log(error.message);
+    })
+    .on("end", function () {
+      console.log(data);
+      console.log("finished");
+    });
 
-    const courses = await Course.insertMany(data);
-    for (newCourse of courses) {
+  db.once("open", async () => {
+    try {
+      await Course.deleteMany({});
+
+      const courses = await Course.insertMany(data);
+      for (newCourse of courses) {
+      }
+
+      console.log("all done!");
+      process.exit(0);
+    } catch (error) {
+      throw error;
     }
+  });
+};
 
-    console.log("all done!");
-    process.exit(0);
-  } catch (error) {
-    throw error;
-  }
-});
+const uploadStudentData = (studentData) => {
+  fs.createReadStream(studentData)
+    .pipe(parse({ delimiter: ",", columns: true, ltrim: true }))
+    .on("data", function (row) {
+      data.push(row);
+    })
+    .on("error", function (error) {
+      console.log(error.message);
+    })
+    .on("end", function () {
+      console.log(data);
+      console.log("finished");
+    });
 
-// const uploadCsv = (courses) => {
-//   const courseCsv = courses;
-// };
+  db.once("open", async () => {
+    try {
+      await Student.deleteMany({});
 
-// module.exports = { uploadCsv };
+      const courses = await Student.insertMany(data);
+      for (newCourse of courses) {
+      }
+
+      console.log("all done!");
+      process.exit(0);
+    } catch (error) {
+      throw error;
+    }
+  });
+};
+
+module.exports = { uploadCourseData, uploadStudentData };
