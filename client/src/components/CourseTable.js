c1fe596fef03e5e54b4a44987649fe7fa92cb0c7
Queen McSteve <niko.desilva@esmt.org>
2022-08-26 19:54:19 +0200

    add front end edit course info functionality, part 1

--- client/src/components/CourseTable.js
+++ client/src/components/CourseTable.js
@@ -1,41 +1,56 @@
 import * as React from "react";
+import { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 const { useQuery } = require("@apollo/client");
 const { COURSES } = require("../utils/queries");
 
-const columns = [
-  {
-    field: "id",
-    headerName: "ID",
-    width: 70,
-    // TRY reformatting ID into simple index
-    // valueFormatter: (id) => {
-    //     const idFormatted =
-    // },
-  },
-  { field: "name", headerName: "Name", width: 160 },
-  {
-    field: "ects",
-    headerName: "ECTS Weight",
-    type: "number",
-    width: 100,
-  },
-  {
-    field: "capacity",
-    headerName: "Capacity",
-    width: 130,
-    editable: true,
-  },
-  {
-    field: "demand",
-    headerName: "Demand",
-    width: 130,
-    editable: true,
-  },
-];
-
 export default function CourseTable() {
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      width: 70,
+      // TRY reformatting ID into simple index
+      // valueFormatter: (id) => {
+      //     const idFormatted =
+      // },
+    },
+    { field: "name", headerName: "Name", width: 160 },
+    {
+      field: "ects",
+      headerName: "ECTS Weight",
+      type: "number",
+      width: 100,
+    },
+    {
+      field: "capacity",
+      headerName: "Capacity",
+      width: 130,
+      editable: true,
+    },
+    {
+      field: "demand",
+      headerName: "Demand",
+      width: 130,
+      editable: true,
+    },
+    {
+      field: "edit",
+      headerName: "Edit",
+      disableClickEventBubbling: true,
+      renderCell: (params) => {
+        return <button onClick={() => showCourseModal(params)}>Edit</button>;
+      },
+    },
+  ];
+
+  const showCourseModal = (params) => {
+    // enable modal and set values for current course
+    setShowModal(true);
+  };
+
   const { loading, data, error } = useQuery(COURSES);
+  const [showModal, setShowModal] = useState(false);
   if (loading) {
     return <div>loading</div>;
   } else if (error) {
@@ -47,6 +62,7 @@ export default function CourseTable() {
   }
   return (
     <>
+      {showModal ? <div>Showing Modal</div> : <></>}
       <div style={{ height: 800, width: "100%" }}>
         <DataGrid
           rows={data.courses}
