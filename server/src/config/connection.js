eea1e64cd6529a7608e8da4c680d443253f0eae9
Queen McSteve <niko.desilva@esmt.org>
2022-10-05 16:36:41 +0200

    try adjusting connection.js; troubleshooting server connection from heroku

--- server/src/config/connection.js
+++ server/src/config/connection.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/course-db", {
+mongoose.connect(process.env.MONGODB_URI , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
